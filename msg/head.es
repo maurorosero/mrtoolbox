313d410f671ae27e7b19d120699d9a7c6c2ef53d
Mauro Rosero P. <mauro@rosero.one>
2024-10-06 04:32:37 -0500

    [IMPROVED] Agregamos funcionalidad para instalar paquetes por snap

---
+++ msg/head.es
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#bash script     : head.es 
+#apps            : MRosero Personal Developer Utilities
+#description     : Head Translate Messages (es)
+#author		     : MRP/mrp - Mauro Rosero P.
+#company email   : mauro@rosero.one
+#personal email  : mauro.rosero@gmail.com
+#date            : 20240201
+#version         : 1.0.2
+#notes           :
+#==============================================================================
+#==============================================================================
+
+head_000="Utilitarios de Mauro Rosero P."
+head_001="El comando 'dialog' no está instalado. Por favor, ejecute bootstrap.sh."
+head_installing="Instalando"
+head_info="INFO"
+head_warning="ADVERTENCIA"
+head_error="ERROR"
+head_unknow="UNKNOW"
+head_ubuntu="ubuntu"
+head_confirm="(confirmar)"
+head_os_error="Operación Cancelada. No se pudo determinar el sistema operativo."
+head_op_error="Operación cancelada por el operador!"
+head_pause="Presiona Enter para continuar..."
+head_error_unknow="Operación Cancelada. Se produjo un error desconocido!"
+head_fail="Fail"
+head_gpg_subkey_none="[GNUPG:] SUBKEY_ID None"
+head_gpg_subkey_id="[GNUPG:] SUBKEY_ID"
+head_gpg_reference="[GNUPG:] REFERENCE SUBKEYS RC"
+head_gpg_no_pkey="[GNUPG:] NO_PKEY"
+head_gpg_export="[GNUPG:] EXPORT OK"
+head_gpg_dbtables_ok="[GNUPG:] EXPORT SQLITE DB TABLES OK"
+head_gpg_revoke_copy_ok="[GNUPG:] COPY TEMPORAL REVOKE FILES OK"
+head_gpg_zip_ok="[GNUPG:] SECURE ZIP FILE OK"
+head_key_end="999999END"
+head_exit="Finalizar"
+head_create="Adicionar"
+head_delete="Eliminar"
+
+vldt_001="no puede estar en blanco..."
+vldt_002="Contraseña incorrecta! Intente nuevamente."
+vldt_010="Dirección de correo electrónico es inválida!"
+vldt_011="Duración de clave gpg inválida!"
+vldt_012="No es un nombre de dominio DNS válido!"
+
+label_email="Correo Electrónico:"
+
---
+++ packs_snap.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+#bash script     : packs_snap.sh
+#apps            : MRosero Personal Developer Utilities
+#description     : Snapd Packages Installer
+#author		     : MRP/mrp - Mauro Rosero P.
+#company email   : mauro@rosero.one
+#personal email  : mauro.rosero@gmail.com
+#date            : 20240201
+#version         : 1.0.2
+#notes           :
+#==============================================================================
+#==============================================================================
+
+install() {
+    local install_home=$1
+    local install_file=$2
+
+    # Load dialog console library
+    source "${HOME}/bin/lib/messages.lib"
+
+    # Load Installer Functions
+    source "${install_home}/bin/lib/console.lib"
+
+    # Load Installer Functions
+    source "${install_home}/bin/lib/packages.lib"
+
+    # Load headers messages
+    load_messages "${install_home}" "head"
+
+    # Load packages messages
+    load_messages "${install_home}" "packages"
+    title="${head_000}"
+    apps_title="${pkmsg_300}"
+
+    # Install Base Packages with Snap
+    snapd_packages ${install_file}
+}
+
+# Main.- Llamar a la función con sudo
+clear
+# Load dialog console library
+source "${HOME}/bin/lib/messages.lib"
+
+# Load dialog console library
+source "${HOME}/bin/lib/console.lib"
+
+# Load os packages library
+source "${HOME}/bin/lib/packages.lib"
+
+# Load headers messages
+load_messages "${HOME}" "head"
+
+# Load packages messages
+load_messages "${HOME}" "packages"
+title="${head_000}"
+apps_title="${pkmsg_300}"
+
+# Check if dialog is not installed, exited!
+if ! command -v dialog >/dev/null 2>&1
+then
+    clear
+    echo "${head_000}"
+    echo "------------------------------------------------------------------------------"
+    echo >&2 "${head_001}"
+    exit 1
+fi
+
+get_osname
+if [ "${os_name}" == "${head_unknow}" ]
+then
+    show_error_dialog "${os_name} ${head_error}" "${pkmsg_004}"
+    exit 2
+else
+    if [ "${os_name}" != "${head_ubuntu}" ]
+    then
+        show_error_dialog "${os_name} ${head_error}" "${pkmsg_009}"
+        exit 2
+    fi
+fi
+
+mz_yesno "${pkmsg_010}"
+if [ "${result}" == "0" ]
+then
+    base_apps_file="snap.bin"
+    apps_file="${HOME}/bin/lib/${base_apps_file}"
+    if [ -f "${HOME}/${base_apps_file}" ]
+    then
+        apps_file="${HOME}/${base_apps_file}"
+    fi
+    if [ -f "${apps_file}" ]
+    then
+        mz_inputpass "${pklbl_001}" "${pkhlp_001}"
+        if [ "${codex}" == "0" ]
+        then
+            if [ -n "${value}" ]
+            then
+                clear
+                echo "${value}" | sudo -S bash -c "$(declare -f install); install ${HOME} ${apps_file}"
+                if [ "$?" == "0" ]
+                then
+                    read
+                    clear
+                else
+                    show_error_dialog "${head_error}" "${pkmsg_008}"
+                fi
+            else
+                show_error_dialog "${head_error}" "${pkmsg_007}"
+            fi
+        else
+            show_error_dialog "${head_error}" "${pkmsg_006}"
+        fi
+    else
+        show_error_dialog "${head_error}" "${pkmsg_005}"
+    fi
+else
+    clear
+fi
